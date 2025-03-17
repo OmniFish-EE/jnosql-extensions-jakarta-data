5e3be0c3bb61d1c2c11f8d6f6d1a19933f43f2e2
Ondro Mihalyi <mihalyi@omnifish.ee>
2025-03-17 23:02:51 +0100

    feat: Jakarta Persistence Driver - Contains, EndsWith
    Also fix unit tests and add test for findByNameIgnoreCaseNot

--- jnosql-jakarta-persistence/jnosql-jakarta-persistence-data-tck-runner/src/test/java/org/eclipse/jnosql/tck/jakartapersistence/SelectedJNoSqlEntityTests.java
+++ jnosql-jakarta-persistence/jnosql-jakarta-persistence-data-tck-runner/src/test/java/org/eclipse/jnosql/tck/jakartapersistence/SelectedJNoSqlEntityTests.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-
 /**
  *
  * @author Ondro Mihalyi
@@ -30,395 +29,83 @@ import org.junit.jupiter.api.Test;
 @EnableAutoWeld
 class SelectedJNoSqlEntityTests extends JNoSqlEntityTests {
 
-    @Override
     @Test
-    public void testLiteralInteger() {
-        super.testLiteralInteger();
+    @Override
+    // Ignore case (findByHexadecimalIgnoreCase)
+    public void testSingleEntity() {
+        super.testSingleEntity();
     }
 
-    @Override
     @Test
-    public void testEmptyQuery() {
-        super.testEmptyQuery();
+    @Override
+    // AND condition (findByHexadecimalContainsAndIsControlNot)
+    public void testContainsInString() {
+        super.testContainsInString();
     }
 
-    @Override
     @Test
-    public void testLiteralString() {
-        super.testLiteralString();
-    }
-
     @Override
-    @Test
-    public void testLiteralTrue() {
-        super.testLiteralTrue();
-    }
-
-    @Override
-    @Test
-    public void testPartialQuerySelectAndOrderBy() {
-        super.testPartialQuerySelectAndOrderBy();
-    }
-
-    @Override
-    @Test
-    public void testQueryWithNot() {
-        super.testQueryWithNot();
-    }
-
-    @Override
-    @Test
-    public void testDefaultMethod() {
-        super.testDefaultMethod();
-    }
-
-    @Override
-    @Test
-    public void testUpdateQueryWithoutWhereClause() {
-        super.testUpdateQueryWithoutWhereClause();
-    }
-
-    @Override
-    @Test
-    public void testFindPage() {
-        super.testFindPage();
-    }
-
-    @Override
-    public void testVarargsSort() {
-        super.testVarargsSort();
-    }
-
-    @Override
-    @Test
-    public void testUpdateQueryWithWhereClause() {
-        super.testUpdateQueryWithWhereClause();
-    }
-
-    @Override
-    public void testTrue() {
-        super.testTrue();
-    }
-
-    @Override
-    @Test
-    // paging
-    public void testThirdAndFourthSlicesOf5() {
-        super.testThirdAndFourthSlicesOf5();
-    }
-
-    @Override
-    public void testThirdAndFourthPagesOf10() {
-        super.testThirdAndFourthPagesOf10();
-    }
-
-    @Override
-    @Test
-    public void testStreamsFromList() {
-        super.testStreamsFromList();
-    }
-
-    @Override
-    public void testStaticMetamodelDescendingSortsPreGenerated() {
-        super.testStaticMetamodelDescendingSortsPreGenerated();
-    }
-
-    @Override
-    @Test
     public void testStaticMetamodelDescendingSorts() {
         super.testStaticMetamodelDescendingSorts();
     }
 
-    @Override
     @Test
-    public void testStaticMetamodelAttributeNamesPreGenerated() {
-        super.testStaticMetamodelAttributeNamesPreGenerated();
-    }
-
-    @Override
-    @Test
-    public void testStaticMetamodelAttributeNames() {
-        super.testStaticMetamodelAttributeNames();
-    }
-
     @Override
     public void testStaticMetamodelAscendingSortsPreGenerated() {
         super.testStaticMetamodelAscendingSortsPreGenerated();
     }
 
-    @Override
-    public void testStaticMetamodelAscendingSorts() {
-        super.testStaticMetamodelAscendingSorts();
-    }
-
-    @Override
     @Test
-    public void testSliceOfNothing() {
-        super.testSliceOfNothing();
-    }
-
     @Override
-    public void testSingleEntity() {
-        super.testSingleEntity();
-    }
-
-    @Override
-    public void testQueryWithParenthesis() {
-        super.testQueryWithParenthesis();
-    }
-
-    @Override
-    public void testQueryWithOr() {
-        super.testQueryWithOr();
-    }
-
-    @Override
-    public void testQueryWithNull() {
-        super.testQueryWithNull();
-    }
-
-    @Override
-    @Test
     public void testPrimaryEntityClassDeterminedByLifeCycleMethods() {
         super.testPrimaryEntityClassDeterminedByLifeCycleMethods();
     }
 
-    @Override
     @Test
-    public void testPartialQueryOrderBy() {
-        super.testPartialQueryOrderBy();
+    @Override
+    public void testLiteralTrue() {
+        super.testLiteralTrue();
     }
 
-    @Override
-    public void testPageOfNothing() {
-        super.testPageOfNothing();
-    }
-
-    @Override
-    public void testOrderByHasPrecedenceOverSorts() {
-        super.testOrderByHasPrecedenceOverSorts();
-    }
-
-    @Override
-    public void testOrderByHasPrecedenceOverPageRequestSorts() {
-        super.testOrderByHasPrecedenceOverPageRequestSorts();
-    }
-
-    @Override
     @Test
-    public void testOr() {
-        super.testOr();
-    }
-
-    @Override
-    public void testNot() {
-        super.testNot();
-    }
-
-    @Override
-    @Test
-    public void testNonUniqueResultException() {
-        super.testNonUniqueResultException();
-    }
-
-    @Override
-    public void testMixedSort() {
-        super.testMixedSort();
-    }
-
-    @Override
-    public void testLiteralEnumAndLiteralFalse() {
-        super.testLiteralEnumAndLiteralFalse();
-    }
-
-    @Override
-    public void testLimitToOneResult() {
-        super.testLimitToOneResult();
-    }
-
-    @Override
-    public void testLimitedRange() {
-        super.testLimitedRange();
-    }
-
-    @Override
-    public void testLimit() {
-        super.testLimit();
-    }
-
-    @Override
-    @Test
-    public void testLessThanWithCount() {
-        super.testLessThanWithCount();
-    }
-
-    @Override
-    @Test
-    public void testCursoredPageWithoutTotalOfNothing() {
-        super.testCursoredPageWithoutTotalOfNothing();
-    }
-
-    @Override
-    @Test
-    public void testCursoredPageWithoutTotalOf9FromCursor() {
-        super.testCursoredPageWithoutTotalOf9FromCursor();
-    }
-
-    @Override
-    @Test
-    public void testCursoredPageOfNothing() {
-        super.testCursoredPageOfNothing();
-    }
-
-    @Override
-    @Test
-    public void testCursoredPageOf7FromCursor() {
-        super.testCursoredPageOf7FromCursor();
-    }
-
     @Override
     public void testIgnoreCase() {
         super.testIgnoreCase();
     }
 
-    @Override
-    public void testIn() {
-        super.testIn();
-    }
-
-    @Override
     @Test
+    @Override
     public void testGreaterThanEqualExists() {
         super.testGreaterThanEqualExists();
     }
 
-    @Override
-    public void testFirstSliceOf5() {
-        super.testFirstSliceOf5();
-    }
-
-    @Override
-    public void testFirstPageOf10() {
-        super.testFirstPageOf10();
-    }
-
-    @Override
     @Test
-    public void testFirstCursoredPageWithoutTotalOf6AndNextPages() {
-        super.testFirstCursoredPageWithoutTotalOf6AndNextPages();
-    }
-
-    @Override
-    @Test
-    public void testFirstCursoredPageOf8AndNextPages() {
-        super.testFirstCursoredPageOf8AndNextPages();
-    }
-
-    @Override
-    @Test
-    public void testFindOptional() {
-        super.testFindOptional();
-    }
-
-    @Override
-    @Test
-    public void testFindOne() {
-        super.testFindOne();
-    }
-
-    @Override
-    public void testFindList() {
-        super.testFindList();
-    }
-
     @Override
     public void testFindFirst3() {
         super.testFindFirst3();
     }
 
+    @Test
     @Override
     public void testFindFirst() {
         super.testFindFirst();
     }
 
-    @Override
-    public void testFindAllWithPagination() {
-        super.testFindAllWithPagination();
-    }
-
-    @Override
-    public void testFinalSliceOfUpTo5() {
-        super.testFinalSliceOfUpTo5();
-    }
-
-    @Override
-    public void testFinalPageOfUpTo10() {
-        super.testFinalPageOfUpTo10();
-    }
-
-    @Override
     @Test
-    public void testFalse() {
-        super.testFalse();
-    }
-
     @Override
     public void testEmptyResultException() {
         super.testEmptyResultException();
     }
 
-    @Override
-    public void testDescendingSort() {
-        super.testDescendingSort();
-    }
-
+    @Test
     @Override
     public void testDataRepository() {
         super.testDataRepository();
     }
 
-    @Override
-    public void testContainsInString() {
-        super.testContainsInString();
-    }
-
-    @Override
     @Test
+    @Override
     public void testCommonInterfaceQueries() {
         super.testCommonInterfaceQueries();
     }
-
-    @Override
-    @Test
-    public void testBy() {
-        super.testBy();
-    }
-
-    @Override
-    public void testBeyondFinalSlice() {
-        super.testBeyondFinalSlice();
-    }
-
-    @Override
-    public void testBeyondFinalPage() {
-        super.testBeyondFinalPage();
-    }
-
-    @Override
-    @Test
-    public void testBasicRepositoryMethods() {
-        super.testBasicRepositoryMethods();
-    }
-
-    @Override
-    @Test
-    public void testBasicRepositoryBuiltInMethods() {
-        super.testBasicRepositoryBuiltInMethods();
-    }
-
-    @Override
-    public void testBasicRepository() {
-        super.testBasicRepository();
-    }
-
 }
